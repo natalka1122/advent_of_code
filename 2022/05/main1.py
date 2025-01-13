a99c65b7a04fb836f35bdc09d5a08ea397ae905d
Natalia Konovalova <natalka1122@gmail.com>
2025-01-13 18:47:06 +0100

    2016-05 done

---
+++ 2022/05/main1.py
@@ -0,0 +1,45 @@
+import re
+
+FILENAME = "demo.txt"  # expected CMZ
+FILENAME = "input.txt"
+
+
+def main() -> None:
+    crates: list[list[str]] = []
+    is_rearrangement = False
+    with open(FILENAME, "r") as file:
+        for line in file:
+            if len(line) == 1:
+                if is_rearrangement:
+                    raise NotImplementedError
+                is_rearrangement = True
+                for i in range(len(crates)):
+                    crates[i] = crates[i][::-1]
+                # print(crates)
+            elif is_rearrangement:
+                line_match = re.match(r"move (\d+) from (\d+) to (\d+)", line.strip())
+                if line_match is None:
+                    raise NotImplementedError
+                count = int(line_match.group(1))
+                source = int(line_match.group(2)) - 1
+                target = int(line_match.group(3)) - 1
+                for i in range(count):
+                    crates[target].append(crates[source].pop())
+                # print(crates)
+            else:
+                if "[" in line:
+                    for i in range((len(line)) // 4):
+                        symbol = line[i * 4 + 1]
+                        if len(crates) == i:
+                            crates.append([])
+                        if symbol != " ":
+                            crates[i].append(symbol)
+                else:
+                    continue
+    if not is_rearrangement:
+        raise NotImplementedError
+    print("".join(map(lambda x: x[-1], crates)))
+
+
+if __name__ == "__main__":
+    main()
