27c9fc2fe75d1710e162475aaf07f5b0af08e463
Natalia Konovalova <natalka1122@gmail.com>
2025-01-13 20:10:57 +0100

    2016-07 done

---
+++ 2022/07/main1.py
@@ -0,0 +1,74 @@
+from __future__ import annotations
+
+FILENAME = "demo.txt"  # expected 95437
+FILENAME = "input.txt"
+
+MAX = 100000
+
+
+class File:
+    def __init__(self, name: str, size: int) -> None:
+        self.name = name
+        self.size = size
+
+
+class Directory:
+    def __init__(self, parent: Directory | None = None) -> None:
+        self.parent = parent
+        self.files: set[File] = set()
+        self.dirs: dict[str, Directory] = dict()
+
+    def add_file(self, filesize: int, filename: str) -> None:
+        self.files.add(File(filename, filesize))
+
+    def add_dir(self, name: str) -> None:
+        self.dirs[name] = Directory(parent=self)
+
+    @property
+    def size(self) -> int:
+        files_size = sum(map(lambda x: x.size, self.files))
+        dirs_size = sum(map(lambda x: x.size, self.dirs.values()))
+        return files_size + dirs_size
+
+
+def main() -> None:
+    root = Directory()
+    current_directory = root
+    dirs = []
+    with open(FILENAME, "r") as file:
+        for line_ in file:
+            line = line_.strip()
+            print(line)
+            if line == "$ cd /":
+                current_directory = root
+                is_ls = False
+            elif line == "$ ls":
+                is_ls = True
+            elif line.startswith("$ cd "):
+                cd = line.split(" ")[2]
+                if cd == "..":
+                    if current_directory.parent is None:
+                        raise NotImplementedError
+                    current_directory = current_directory.parent
+                else:
+                    current_directory = current_directory.dirs[cd]
+            elif is_ls:
+                if line.startswith("dir "):
+                    dirname = line.split(" ")[1]
+                    current_directory.add_dir(dirname)
+                    dirs.append(current_directory.dirs[dirname])
+                else:
+                    filesize, filename = line.split(" ")
+                    current_directory.add_file(int(filesize), filename)
+
+            else:
+                raise NotImplementedError
+    result = 0
+    for directory in dirs:
+        if directory.size <= MAX:
+            result += directory.size
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
