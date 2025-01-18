0262257f329c81d8f77c9b94156c4fefa3fbe203
Natalia Konovalova <natalka1122@gmail.com>
2025-01-18 17:33:03 +0100

    2022-15 done

--- 2022/15/main2.py
+++ 2022/15/main2.py
@@ -5,27 +5,55 @@ FILENAME, MAX_COORD = "input.txt", 4000000
 
 
 def main() -> None:
-    result = set()
-    for x in range(MAX_COORD):
-        if x % 10 == 0 :
-            print(x)
-        for y in range(MAX_COORD):
-            pass
-    # with open(FILENAME, "r") as file:
-    #     for line in file:
-    #         line_match = re.match(
-    #             r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)",
-    #             line.strip(),
-    #         )
-    #         if line_match is None:
-    #             raise NotImplementedError
-    #         sx, sy, bx, by = map(int, line_match.groups())
-    #         dist = abs(sx - bx) + abs(sy - by)
-    #         if dist >= abs(sy - TARGET):
-    #             print(sx, sy, bx, by)
-    #         for x in range(sx - dist + abs(sy - TARGET), sx + dist - abs(sy - TARGET)):
-    #             result.add(x)
-    print(result)
+    lines = []
+    with open(FILENAME, "r") as file:
+        for line in file:
+            line_match = re.match(
+                r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)",
+                line.strip(),
+            )
+            if line_match is None:
+                raise NotImplementedError
+            lines.append(tuple(map(int, line_match.groups())))
+    for y in range(MAX_COORD + 1):
+        cuts: list[tuple[int, int]] = []
+        for sx, sy, bx, by in lines:
+            dist = abs(sx - bx) + abs(sy - by)
+            if dist >= abs(sy - y):
+                c1 = max(0, sx - dist + abs(sy - y))
+                c2 = min(MAX_COORD, sx + dist - abs(sy - y))
+                if c1 > c2:
+                    continue
+                if len(cuts) == 0:
+                    cuts.append((c1, c2))
+                else:
+                    cuts.append((c1, c2))
+                    has_changed = True
+                    while has_changed:
+                        has_changed = False
+                        for index1 in range(len(cuts)):
+                            for index2 in range(len(cuts)):
+                                if index1 >= index2:
+                                    continue
+                                if cuts[index1][1] + 1 < cuts[index2][0]:
+                                    continue
+                                if cuts[index2][1] + 1 < cuts[index1][0]:
+                                    continue
+                                cuts[index1] = min(
+                                    cuts[index1][0], cuts[index2][0]
+                                ), max(cuts[index1][1], cuts[index2][1])
+                                cuts.pop(index2)
+                                has_changed = True
+                                break
+                            if has_changed:
+                                break
+        if len(cuts) == 0 or len(cuts) > 2:
+            raise NotImplementedError
+        if len(cuts) == 2:
+            break
+    if len(cuts) != 2:
+        raise NotImplementedError
+    print(y + 4000000 * (cuts[0][1] + 1))
 
 
 if __name__ == "__main__":
