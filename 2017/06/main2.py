7e35ba0bf13d355dad11ceeba2fb828f75c40f11
Natalia Konovalova <natalka1122@gmail.com>
2025-01-28 00:35:35 +0100

    2017-06 done

---
+++ 2017/06/main2.py
@@ -0,0 +1,27 @@
+FILENAME = "demo.txt"  # expected 4
+FILENAME = "input.txt"
+
+
+def main() -> None:
+    state_list: list[tuple[int, ...]] = []
+    with open(FILENAME, "r") as file:
+        for line in file:
+            state_list.append(tuple(map(int, line.strip().replace("\t", " ").split(" "))))
+    print(state_list)
+    while True:
+        next_state = list(state_list[-1])
+        max_bank = max(next_state)
+        max_bank_index = next_state.index(max_bank)
+        next_state[max_bank_index] = 0
+        for i in range(max_bank):
+            next_state[(max_bank_index + 1 + i) % len(next_state)] += 1
+        next_state_tuple = tuple(next_state)
+        if next_state_tuple in state_list:
+            break
+        else:
+            state_list.append(next_state_tuple)
+    print(len(state_list) - state_list.index(next_state_tuple))
+
+
+if __name__ == "__main__":
+    main()
